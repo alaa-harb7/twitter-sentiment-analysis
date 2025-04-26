8a2b8dca13c3d0c97617e38a4240cc19503a6c6f
alaa-harb7 <alaaharbofficial@gmail.com>
2025-04-26 15:10:11 +0300

    update mian.py and requirements.txt

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import joblib
+import os
+import gdown  
+
+# Model and Vectorizer URLs
+model_url = "https://drive.google.com/uc?id=1lpGgmJgtFBCKxX9oaGEPC9lv3u1Y3r3Q"
+vectorizer_url = "https://drive.google.com/uc?id=15TKBguU2r1ihDrjrPutwRBQGlXrpW6q1"
+
+def download_file(url, filename):
+    if not os.path.exists(filename):
+        print(f"Downloading {filename}...")
+        gdown.download(url, filename, quiet=False)
+        print(f"{filename} downloaded.")
+
+# Upload Model and Vectorizer From Drive
+download_file(model_url, "sentiment_model2.pkl")
+download_file(vectorizer_url, "vectorizer.pkl")
 
 # Load model and vectorizer
 model = joblib.load("sentiment_model2.pkl")
@@ -12,7 +28,7 @@ vectorizer = joblib.load("vectorizer.pkl")
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# ✅ التصنيفات الأربعة
+# 4 Categories
 label_map = {
     3: "Positive",
     1: "Negative",
